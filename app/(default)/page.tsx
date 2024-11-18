8d9a8d0398f3fe069749711fe65b2df81e8049e3
Robin Pohlman <robin.pohlman@gmail.com>
2024-11-18 15:32:30 +0100

    feat: setup website structure

---
+++ app/(default)/page.tsx
@@ -0,0 +1,12 @@
+import LandingPage from '@/components/landing-page';
+import Header from '@/components/ui/header';
+import Footer from '@/components/ui/footer';
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <LandingPage/>
+    <Footer/>
+    </>
+  )
+}
---
+++ components/landing-page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function LandingPage() { 
+  return (
+    <div className="landing-page">
+      <h1>This is the landing page</h1>
+    </div>
+
+  );
+}
